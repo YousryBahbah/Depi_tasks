74760e353767134fd5b8ebceccf1595a97f950d3
YousryBahbah <yousrybahbah04@gmail.com>
2025-07-30 17:40:28 +0300

    c#_task1

---
+++ task1_c#/task1_c#/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Concurrent;
+
+namespace task1_c_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            Console.WriteLine("Input the first number: ");
+            decimal Fnum = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Input the second number: ");
+            decimal Snum = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("What do you want to do with those numbers?");
+            Console.WriteLine("[A]dd");
+            Console.WriteLine("[S]ubtract");
+            Console.WriteLine("[M]ultiply");
+            char op = Convert.ToChar(Console.ReadLine());
+            if (op == 'A' || op == 'a')
+            {
+                decimal num = Fnum + Snum;
+                Console.WriteLine($"{Fnum} + {Snum} = {num}"); 
+            }
+            else if (op == 'S' || op == 's')
+            {
+                decimal num = Fnum - Snum;
+                Console.WriteLine($"{Fnum} - {Snum} = {num}");
+            }
+            else if (op == 'M' || op == 'm')
+            {
+                decimal num = Fnum * Snum;
+                Console.WriteLine($"{Fnum} * {Snum} = {num}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid option");
+            }
+            Console.ReadKey();
+        }
+    }
+}
