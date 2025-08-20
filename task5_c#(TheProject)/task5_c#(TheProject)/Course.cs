ad1fa5b4aa28dbfa69ef55de6a75efa4dd7e28d1
YousryBahbah <yousrybahbah04@gmail.com>
2025-08-20 22:43:12 +0300

    task5_c#

---
+++ task5_c#(TheProject)/task5_c#(TheProject)/Course.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task5_c__TheProject_
+{
+    internal class Course
+    {
+        string _Title;
+        string _Description;
+        string _Maximum_degree;
+        public string Title { get => _Title; set => _Title = value; }
+        public string Description { get => _Description; set => _Description = value; }
+        public string Maximum_degree { get => _Maximum_degree; set => _Maximum_degree = value; }
+
+        public List<Students> EnrolledStudent { get; set; } = new List<Students>();
+        public List<Instructor> Instructor { get; set; } = new List<Instructor>();
+        public List<Questions> Question { get; set; } = new List<Questions>();
+        public List<Exam> Exams { get; set; } = new List<Exam>();
+
+    }
+}
---
+++ task5_c#(TheProject)/task5_c#(TheProject)/Questions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task5_c__TheProject_
+{
+    internal class Questions
+    {
+        int _id;
+        string _text;
+        string _marks;
+
+        public int Id { get => _id; set => _id = value; }
+        public string Text { get => _text; set => _text = value; }
+        public string Marks { get => _marks; set => _marks = value; }
+
+    }
+
+    internal class MCQ : Questions
+    {
+        string _Option1;
+        string _Option2;
+        string _Option3;
+        string _Option4;
+        string _CorrectAnswer;
+
+        public string Option1 { get => _Option1; set => _Option1 = value; }
+        public string Option2 { get => _Option2; set => _Option2 = value; }
+        public string Option3 { get => _Option3; set => _Option3 = value; }
+        public string Option4 { get => _Option4; set => _Option4 = value; }
+        public string CorrectAnswer { get => _CorrectAnswer; set => _CorrectAnswer = value; }
+    }
+
+    internal class TrueFalse : Questions
+    {
+        string _CorrectAnswer;
+        public string CorrectAnswer { get => _CorrectAnswer; set => _CorrectAnswer = value; }
+    }
+
+    internal class Essay : Questions
+    {
+
+    }
+
+
+}
