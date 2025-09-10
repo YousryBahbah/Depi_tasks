678f92e6739153a86cf6cc7ae2a80128e53b013b
YousryBahbah <yousrybahbah04@gmail.com>
2025-09-10 15:41:53 +0300

    EFcore_task1

---
+++ Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EF_task1.Models;
+
+public partial class Course
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public int Credits { get; set; }
+
+    public virtual ICollection<Enrollment> Enrollments { get; set; } = new List<Enrollment>();
+}
---
+++ Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EF_task1.Models;
+
+public partial class Department
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Loc { get; set; }
+
+    public virtual ICollection<Student> Students { get; set; } = new List<Student>();
+}
