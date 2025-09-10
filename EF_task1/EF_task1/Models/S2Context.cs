ec31a78559c7eb499bdf2f49ede89e558cd4cdf7
YousryBahbah <yousrybahbah04@gmail.com>
2025-09-10 15:49:00 +0300

    EFcore_task1

---
+++ EF_task1/EF_task1/Models/S2Context.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_task1.Models;
+
+public partial class S2Context : DbContext
+{
+    public S2Context()
+    {
+    }
+
+    public S2Context(DbContextOptions<S2Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Course> Courses { get; set; }
+
+    public virtual DbSet<Department> Departments { get; set; }
+
+    public virtual DbSet<Enrollment> Enrollments { get; set; }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=YOUSRY-BAHBAH\\SQLEXPRESS;Database=SchoolDB;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Course>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Courses__3214EC27CD9A1DC5");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Title).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Department>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Departme__3214EC27C1645E46");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Loc).HasMaxLength(50);
+            entity.Property(e => e.Name).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Enrollment>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Enrollme__3214EC279110AEDF");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Grade).HasMaxLength(10);
+
+            entity.HasOne(d => d.Course).WithMany(p => p.Enrollments)
+                .HasForeignKey(d => d.CourseId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Enrollments_Courses");
+
+            entity.HasOne(d => d.Student).WithMany(p => p.Enrollments)
+                .HasForeignKey(d => d.StudentId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Enrollments_Students");
+        });
+
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Students__3214EC279537DEB1");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Adddrsss).HasMaxLength(100);
+            entity.Property(e => e.Name).HasMaxLength(50);
+
+            entity.HasOne(d => d.Dept).WithMany(p => p.Students)
+                .HasForeignKey(d => d.DeptId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Students_Departments");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ EF_task1/EF_task1/Program.cs
@@ -0,0 +1,81 @@
+﻿using EF_task1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //Scaffold - DbContext "Server=.;Database=SchoolDB;Trusted_Connection=True;TrustServerCertificate=True;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models -Context S2context
+            //enumerable vs queriable
+            S2Context c = new S2Context();
+            
+            //Ienumerable VS Iquerable
+            //querable
+            var students = c.Students.Where(x => x.DeptId == 1);
+            foreach (var student in students)
+            {
+                Console.WriteLine($"the name is {student.Name} ID : {student.Id} ");
+            }
+            Console.WriteLine("===================================");
+            //enumerable
+            var students2 = c.Students.ToList().Where(x => x.DeptId == 1);
+            foreach (var student in students2)
+            {
+                Console.WriteLine($"the name is {student.Name} ID : {student.Id} ");
+            }
+            Console.WriteLine("===================================");
+            //include
+            S2Context c2 = new S2Context();
+            var students3 = c2.Students.Include(x => x.Dept);
+            foreach (var student in students3)
+            {
+                Console.WriteLine($"the name is {student.Name} ID : {student.Id} dept : {student.Dept.Name}");
+            }
+            Console.WriteLine("===================================");
+            var dept = c2.Departments.Include(x => x.Students);
+            foreach (var d in dept)
+            {
+                Console.WriteLine($"the  Department is {d.Name} ID : {d.Id}  ");
+                foreach (var student in d.Students)
+                {
+                    Console.WriteLine($"{student.Name} , ({d.Name})");
+                }
+            }
+            Console.WriteLine("===================================");
+            #region tracking
+            S2Context c3 = new S2Context();
+            var DeptToChange = c3.Departments.Find(1);
+            DeptToChange.Name = ".Net";
+            Console.WriteLine(c3.Entry(DeptToChange).State);
+            c3.SaveChanges();
+            var DeptToNotChange = c3.Departments.AsNoTracking().SingleOrDefault(x => x.Id == 2);
+            DeptToNotChange.Name = "Software Development";
+            Console.WriteLine(c3.Entry(DeptToNotChange).State);
+            c3.SaveChanges();
+            #endregion
+            #region CRUD
+            S2Context c4 = new S2Context();
+            var deptadd = new Department() { Name = "NodeJS", Loc = "Alex" };
+            c4.Departments.Add(deptadd);
+            c4.SaveChanges();
+            var showdepts = c4.Departments;
+            foreach(var d in showdepts)
+            {
+                Console.WriteLine($"{d.Id} , {d.Name} ");
+            }
+            var studentUpdate = c4.Students.Find(3);
+            studentUpdate.Name = "Yousry";
+            studentUpdate.Age = 21;
+            c4.SaveChanges();
+            var todeleteAdepartment = c4.Departments.Find(3);
+            c4.Departments.Remove(todeleteAdepartment);
+            #endregion
+
+        }
+
+    }
+}
